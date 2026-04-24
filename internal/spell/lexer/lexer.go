a455358d18388cd81345eabfdf1f07a5f8904ef0
Alan Lima <alanlimanvc@gmail.com>
2026-04-24 17:11:31 -0300

    feat(lexer): create lexer

---
+++ internal/spell/lexer/lexer.go
@@ -0,0 +1,174 @@
+package lexer
+
+import (
+	"strings"
+
+	"github.com/alan-b-lima/sun/internal/spell/scanner"
+)
+
+type Stream struct {
+	src *scanner.Source
+
+	token   Token
+	content string
+
+	await  bool
+	insert bool
+}
+
+func NewStream(src *scanner.Source) *Stream {
+	return &Stream{
+		src:   src,
+		token: Illegal,
+	}
+}
+
+func (s *Stream) Next() {
+	for {
+		switch rune := s.next_rune(); {
+		case rune == scanner.EOF:
+			if s.insert_semicolon_after() {
+				return
+			}
+
+			s.token = EOF
+			return
+
+		case is_identifier_char(rune):
+			s.bubble()
+
+			ident := s.next_identifier()
+			if ident == "" {
+				// impossible
+				s.token = Illegal
+				return
+			}
+
+			if keyword, in := keywords[ident]; in {
+				s.token = keyword
+				return
+			}
+
+			s.token = Identifier
+			s.content = ident
+			return
+
+		case rune == '~':
+			s.bubble()
+			ident := s.next_atom()
+			if ident == "" {
+				s.token = Illegal
+				return
+			}
+
+			s.token = Atom
+			s.content = ident
+			return
+
+		case rune == '/':
+			if rune := s.next_rune(); rune != '/' {
+				s.token = Illegal
+				return
+			}
+
+			for s.next_rune() != '\n' {
+			}
+
+			if s.insert_semicolon_after() {
+				return
+			}
+
+		case rune == '}' && s.token != Semicolon:
+			s.token = Semicolon
+			s.bubble()
+			return
+
+		case rune == ';' && s.token == Semicolon:
+
+		case rune == '\n':
+			if s.insert_semicolon_after() {
+				return
+			}
+
+		default:
+			if token, in := punctuation[rune]; in {
+				s.token = token
+				return
+			}
+		}
+	}
+}
+
+func (s *Stream) Token() Token {
+	return s.token
+}
+
+func (s *Stream) At() scanner.Position {
+	return s.src.Position()
+}
+
+func (s *Stream) Content() string {
+	return s.content
+}
+
+func (s *Stream) next_rune() rune {
+	if s.await {
+		s.await = false
+	} else {
+		s.src.Next()
+	}
+
+	return s.src.Rune()
+}
+
+func (s *Stream) bubble() {
+	s.await = true
+}
+
+func (s *Stream) next_identifier() string {
+	var b strings.Builder
+
+	for {
+		rune := s.next_rune()
+		if rune == scanner.EOF {
+			break
+		}
+
+		if !is_identifier_char(rune) {
+			s.bubble()
+			break
+		}
+
+		b.WriteRune(rune)
+	}
+
+	return b.String()
+}
+
+func (s *Stream) next_atom() string {
+	if rune := s.next_rune(); rune != '~' {
+		return ""
+	}
+
+	ident := s.next_identifier()
+	if ident == "" {
+		return ""
+	}
+
+	if rune := s.next_rune(); rune != '~' {
+		s.bubble()
+		return ""
+	}
+
+	return ident
+}
+
+func (s *Stream) insert_semicolon_after() bool {
+	switch s.token {
+	case Identifier, Atom, RParen, RBrace:
+		s.token = Semicolon
+		return true
+	}
+
+	return false
+}
---
+++ internal/spell/lexer/lexer_test.go
@@ -0,0 +1,94 @@
+package lexer_test
+
+import (
+	"testing"
+
+	. "github.com/alan-b-lima/sun/internal/spell/lexer"
+	"github.com/alan-b-lima/sun/internal/spell/scanner"
+)
+
+func TestLexer(t *testing.T) {
+	type TokenEx struct {
+		Token   Token
+		Content string
+	}
+
+	type Test struct {
+		Name string
+		In   string
+		Want []TokenEx
+	}
+
+	tests := []Test{
+		{
+			Name: "many semicolons",
+			In:   `;;;;`,
+			Want: []TokenEx{
+				{Token: Semicolon},
+				{Token: EOF},
+			},
+		},
+		{
+			Name: "atoms",
+			In:   `~air~ ~dirt~ ~water~`,
+			Want: []TokenEx{
+				{Token: Atom, Content: "air"},
+				{Token: Atom, Content: "dirt"},
+				{Token: Atom, Content: "water"},
+				{Token: Semicolon},
+				{Token: EOF},
+			},
+		},
+		{
+			Name: "spell declaration",
+			In:   `spell name { ~air~ 0[~dirt~] release[0] face name }`,
+			Want: []TokenEx{
+				{Token: Spell},
+				{Token: Identifier, Content: "name"},
+				{Token: LBrace},
+				{Token: Atom, Content: "air"},
+				{Token: Identifier, Content: "0"},
+				{Token: LBrack},
+				{Token: Atom, Content: "dirt"},
+				{Token: RBrack},
+				{Token: Release},
+				{Token: LBrack},
+				{Token: Identifier, Content: "0"},
+				{Token: RBrack},
+				{Token: Face},
+				{Token: Identifier, Content: "name"},
+				{Token: Semicolon},
+				{Token: RBrace},
+				{Token: Semicolon},
+				{Token: EOF},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		source := scanner.NewSource([]byte(test.In))
+		stream := NewStream(source)
+
+		for j, want := range test.Want {
+			stream.Next()
+			got := stream.Token()
+
+			if want.Token != got {
+				t.Errorf(
+					"%s @ token %d\nexpected %v\ngot %v\n",
+					test.Name, j, want.Token, got,
+				)
+				continue
+			}
+
+			if want.Content != "" {
+				if content := stream.Content(); want.Content != content {
+					t.Errorf(
+						"%s @ token %d\nexpected\n%v(%s)\ngot %v(%s)\n",
+						test.Name, j, want.Token, want.Content, got, content,
+					)
+				}
+			}
+		}
+	}
+}
