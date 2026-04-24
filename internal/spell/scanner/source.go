423f17d2083b7e3b0d9d25dfe594fa24155ce894
Alan Lima <alanlimanvc@gmail.com>
2026-04-24 17:10:17 -0300

    feat(source): implement source reader

---
+++ internal/spell/scanner/source.go
@@ -0,0 +1,86 @@
+package scanner
+
+import (
+	"fmt"
+	"io"
+	"unicode/utf8"
+)
+
+const (
+	EOF rune = -1
+)
+
+type Source struct {
+	source    []byte
+	character rune
+
+	offset       int
+	line, column int
+}
+
+func NewSource(src []byte) *Source {
+	return &Source{
+		source:    src,
+		character: ' ',
+		offset:    0,
+		line:      1,
+		column:    1,
+	}
+}
+
+func NewSourceFromReader(r io.Reader) (*Source, error) {
+	src, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewSource(src), nil
+}
+
+func (s *Source) Next() {
+	if s.offset >= len(s.source) {
+		s.character = EOF
+		return
+	}
+
+	rune, size := utf8.DecodeRune(s.source[s.offset:])
+	s.offset += size
+
+	if rune == '\n' {
+		s.line++
+		s.column = 0
+	}
+	s.column++
+
+	s.character = rune
+}
+
+func (s *Source) Position() Position {
+	return Position{
+		Offset: s.offset,
+		Line:   s.line,
+		Column: s.column,
+	}
+}
+
+func (s *Source) Rune() rune {
+	return s.character
+}
+
+type Position struct {
+	Offset int
+	Line   int
+	Column int
+}
+
+func (p Position) String() string {
+	if p.Line > 0 {
+		if p.Column > 0 {
+			return fmt.Sprintf("%d:%d", p.Line, p.Column)
+		} else {
+			return fmt.Sprintf("%d", p.Line)
+		}
+	}
+
+	return "-"
+}
